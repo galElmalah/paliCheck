1af6aa8a4433639057325a97d13f9ea46611c2e1
Gal Elmalah <galisrael8914@gmail.com>
2017-11-04 13:24:24 +0200

    fixed some shit, added comments and a main function

--- palindrom.py
+++ palindrom.py
@@ -9,9 +9,15 @@ def reverseString(str):
 
 def palindrom(str):
     pali_count = 0
-    words = [str.split()]
+    words = str.split()
 
     for word in words:
-        if word == reverseString(word):
+        if word == reverseString(word):#check each word with the same word only reversd
             pali_count+=1
     return pali_count
+
+def main():
+    a=input("Enter a string:")
+    print(palindrom(a))
+
+main()
